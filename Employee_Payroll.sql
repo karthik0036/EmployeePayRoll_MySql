b5adecfea2e36b73fa6614e89a82661da1923de6
Karthik <jeefkarthik036@gmail.com>
2022-05-03 09:17:13 +0530

    Created Table and cols

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -3,3 +3,14 @@ create database payroll_services;
 show databases;
 use payroll_services;
 
+-- UC2
+create table employee_payroll
+( id int not null auto_increment ,
+name varchar(50) not null,
+salary Double not null,
+startDate Date not null,
+primary key(id)
+);
+
+desc employee_payroll;
+select * from employee_payroll;
