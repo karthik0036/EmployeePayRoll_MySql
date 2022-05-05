555a89e42eda74d516a370306232ab7bdce39da7
Karthik <jeefkarthik036@gmail.com>
2022-05-05 12:32:52 +0530

    Implementation of ER-Diagrams

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -101,6 +101,90 @@ VALUES
 (7,'Cersi',2000,'2021-03-10','F',NULL,'INDIA','Marketing',0.00,0.00,0.00,6000);
 
 
+-- UC11 Creating ER Diagrams
+-- Employee Table, Company Table, Payroll Table, Department Table(For Joining many to many Relationship), Employee Department Table will be the entities.
+DROP TABLE employee_payroll;
+-- Creating Company Table
+CREATE TABLE CompanyTable
+(
+CompanyID INT NOT NULL AUTO_INCREMENT,
+CompanyName VARCHAR(100),
+PRIMARY KEY (CompanyID)
+);
 
+INSERT INTO CompanyTable
+VALUES
+(1,'APE AUTO'),(2,'BPL INDIA');
 
+SELECT * FROM CompanyTable;
+
+CREATE TABLE EmployeeTable
+(
+EmployeeID int not null auto_increment PRIMARY KEY,
+CompanySelect INT,
+EmployeeName VARCHAR(20) NOT NULL,
+Gender CHAR(1) NOT NULL,
+PhoneNo BIGINT,
+EmployeeAddress VARCHAR(150),
+StartDate DATE NOT NULL,
+FOREIGN KEY (CompanySelect) REFERENCES CompanyTable(CompanyID)
+);
+SELECT * FROM EmployeeTable;
+
+INSERT INTO EmployeeTable
+VALUES
+(1,1,'Rocky','M',1234567671,'Starks North','2018-09-15'),
+(2,1,'Jon','M',9876543243,'Starks  Banner','2019-09-15'),
+(3,2,'Robr','M',3214567893,'Starks Hound','2020-05-12'),
+(4,2,'Arya','F',3456781289,'Starks South','2016-05-09');
+
+-- PayRoll Table Entity
+CREATE TABLE PayrollTable
+(
+BasicPay FLOAT NOT NULL DEFAULT 0.00,
+Deductions FLOAT NOT NULL DEFAULT 0.00,
+TaxablePay FLOAT,
+IncomeTax FLOAT NOT NULL DEFAULT 0.00,
+NetPay FLOAT,
+EmployeeSelect INT,
+FOREIGN KEY (EmployeeSelect) REFERENCES EmployeeTable(EmployeeID)
+);
+SELECT * FROM PayrollTable;
+
+INSERT INTO PayrollTable(BasicPay,Deductions,IncomeTax,EmployeeSelect)
+VALUES
+(200000,20000,10000,1),
+(180000,18000,9000,2),
+(250000,25000,12500,3),
+(300000,30000,15000,4);
+
+-- Updating Values of Deriving Attribute in Payroll Table
+UPDATE PayrollTable SET TaxablePay = (BasicPay - Deductions);
+UPDATE PayrollTable SET NetPay = (TaxablePay - IncomeTax);
+SELECT * FROM PayrollTable;
+
+CREATE TABLE DepartmentTable
+(
+DepartmentID INT NOT NULL auto_increment,
+DepartmentName VARCHAR(100),
+primary key(DepartmentID)
+);
+SELECT * FROM DepartmentTable;
+
+INSERT INTO DepartmentTable(DepartmentName)
+VALUES
+('Sales'),('Marketing'),('Research'),('HR');
+
+CREATE TABLE EmployeeDepartmentTable
+(
+EmployeeSelect INT,
+DepartmentSelect INT,
+FOREIGN KEY (EmployeeSelect) REFERENCES EmployeeTable(EmployeeID),
+FOREIGN KEY (DepartmentSelect) REFERENCES DepartmentTable(DepartmentID)
+);
+SELECT * FROM EmployeeDepartmentTable;
+
+INSERT INTO EmployeeDepartmentTable
+VALUES
+(1,1),(2,2),(3,2),(4,3),(4,4);
 
